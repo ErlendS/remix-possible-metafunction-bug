b7bf261d9fae3c4e5b36b46425844d1483e035a3
Erlend Seppola <erlend.seppola@gmail.com>
2023-10-03 20:20:50 +0200

    test: test that should fail but passes

--- integration/bug-report-test.ts
+++ integration/bug-report-test.ts
@@ -11,39 +11,6 @@ import {
 let fixture: Fixture;
 let appFixture: AppFixture;
 
-////////////////////////////////////////////////////////////////////////////////
-// 💿 👋 Hola! It's me, Dora the Remix Disc, I'm here to help you write a great
-// bug report pull request.
-//
-// You don't need to fix the bug, this is just to report one.
-//
-// The pull request you are submitting is supposed to fail when created, to let
-// the team see the erroneous behavior, and understand what's going wrong.
-//
-// If you happen to have a fix as well, it will have to be applied in a subsequent
-// commit to this pull request, and your now-succeeding test will have to be moved
-// to the appropriate file.
-//
-// First, make sure to install dependencies and build Remix. From the root of
-// the project, run this:
-//
-//    ```
-//    yarn && yarn build
-//    ```
-//
-// Now try running this test:
-//
-//    ```
-//    yarn bug-report-test
-//    ```
-//
-// You can add `--watch` to the end to have it re-run on file changes:
-//
-//    ```
-//    yarn bug-report-test --watch
-//    ```
-////////////////////////////////////////////////////////////////////////////////
-
 test.beforeEach(async ({ context }) => {
   await context.route(/_data/, async (route) => {
     await new Promise((resolve) => setTimeout(resolve, 50));
@@ -53,39 +20,44 @@ test.beforeEach(async ({ context }) => {
 
 test.beforeAll(async () => {
   fixture = await createFixture({
-    ////////////////////////////////////////////////////////////////////////////
-    // 💿 Next, add files to this object, just like files in a real app,
-    // `createFixture` will make an app and run your tests against it.
-    ////////////////////////////////////////////////////////////////////////////
     files: {
       "app/routes/_index.tsx": js`
-        import { json } from "@remix-run/node";
+        import { type MetaFunction, json } from "@remix-run/node";
         import { useLoaderData, Link } from "@remix-run/react";
 
-        export function loader() {
-          return json("pizza");
+        export const meta: MetaFunction<typeof loader> = (loader) => {
+          const metaArray = loader.data?.metaArray
+          if (metaArray) {
+            console.log(metaArray)
+            return metaArray;
+          }
+          return []
+        };
+
+        export async function loader() {
+          const metaArray = [
+            {
+              tagName: 'link',
+              rel: 'icon',
+              href: 'https://remix.run/favicon-32.png',
+              type: 'image/png',
+            },
+          ];
+        
+          return json({ metaArray });
         }
 
         export default function Index() {
-          let data = useLoaderData();
           return (
-            <div>
-              {data}
-              <Link to="/burgers">Other Route</Link>
-            </div>
-          )
-        }
-      `,
-
-      "app/routes/burgers.tsx": js`
-        export default function Index() {
-          return <div>cheeseburger</div>;
+            <main>
+              <h1>Welcome to Remix</h1>
+            </main>
+          );
         }
       `,
     },
   });
 
-  // This creates an interactive app using playwright.
   appFixture = await createAppFixture(fixture);
 });
 
@@ -93,29 +65,15 @@ test.afterAll(() => {
   appFixture.close();
 });
 
-////////////////////////////////////////////////////////////////////////////////
-// 💿 Almost done, now write your failing test case(s) down here Make sure to
-// add a good description for what you expect Remix to do 👇🏽
-////////////////////////////////////////////////////////////////////////////////
-
-test("[description of what you expect it to do]", async ({ page }) => {
+test("Meta component renders meta links with property tagName:'link' as link tags", async ({
+  page,
+}) => {
   let app = new PlaywrightFixture(appFixture, page);
-  // You can test any request your app might get using `fixture`.
-  let response = await fixture.requestDocument("/");
-  expect(await response.text()).toMatch("pizza");
 
-  // If you need to test interactivity use the `app`
-  await app.goto("/");
-  await app.clickLink("/burgers");
-  await page.waitForSelector("text=cheeseburger");
+  await app.goto("/", true);
+  let html = await app.getHtml();
 
-  // If you're not sure what's going on, you can "poke" the app, it'll
-  // automatically open up in your browser for 20 seconds, so be quick!
-  // await app.poke(20);
-
-  // Go check out the other tests to see what else you can do.
+  expect(html).toContain(
+    '<link rel="icon" href="https://remix.run/favicon-32.png" type="image/png" />'
+  );
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// 💿 Finally, push your changes to your fork of Remix and open a pull request!
-////////////////////////////////////////////////////////////////////////////////
